85dd36e2222c356a51aaf976f810d75acd952dd1
anchangsi@gmail.com <anchangsi@gmail.com>
2022-10-14 00:56:53 +0800

    Introduce Environment Variable files

--- dev.sh
+++ dev.sh
@@ -1,11 +1,17 @@
 container_name=jiayuan_devenv
-docker_image_name=base
+docker_image_name=jiayuan_dental_devenv
 source_path=/source/jiayuan_dental
 
 export MSYS_NO_PATHCONV=1
 
 echo_and_run() { echo "\$ $*" ; "$@" ; }
 
+if ! [ -f ".dev.env" ]; then
+  echo "\".dev.env\" is not found. Please copy \".template.env\" as \".dev.env\"."
+  exit 1
+fi
+
+source .dev.env
 
 if ! docker info > /dev/null 2>&1; then
   echo "This script uses docker, and it isn't running - please start docker and try again!"
@@ -18,6 +24,7 @@ if ! [ "$( docker container inspect -f '{{.State.Status}}' $container_name )" ==
    --volume=$(pwd):$source_path \
    -v /var/run/docker.sock:/var/run/docker.sock \
    -p 3306:3306 \
+   --env MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD \
    $docker_image_name:latest
 fi
 
