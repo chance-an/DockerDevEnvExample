6abeebe95372f6968d364154ca0ca8cbe7dc1715
anchangsi@gmail.com <anchangsi@gmail.com>
2022-10-30 22:38:14 +0800

    Sets FOREIGN_KEY_CHECKS values before and after DB creation.

--- db/migrations/20221028142055_user_table.sql
+++ db/migrations/20221028142055_user_table.sql
@@ -1,4 +1,5 @@
 -- migrate:up
+SET FOREIGN_KEY_CHECKS = 0;
 CREATE TABLE users
 (
     id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Primary key',
@@ -9,6 +10,7 @@ CREATE TABLE users
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Created time',
     modified TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Modified time'
 );
+SET FOREIGN_KEY_CHECKS = 1;
 
 -- migrate:down
 DROP TABLE users;
